d4cce8030790228a6f22120adc8532e7790d1126
mevanabey <mevan_ab@hotmail.com>
2023-07-06 16:29:30 +0400

    Updates

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
           <div className="mt-5">
             <h1
               ref={textOne}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+              className="text-4xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
             >
               {data.headerTaglineOne}
             </h1>
@@ -79,13 +79,13 @@ export default function Home() {
             </h1>
             <h1
               ref={textThree}
-              className="text-3xl tablet:text-3xl laptop:text-3xl laptopl:text-3xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+              className="text-lg tablet:text-3xl laptop:text-3xl laptopl:text-3xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
             >
               {data.headerTaglineThree}
             </h1>
             <h1
               ref={textFour}
-              className="text-xl tablet:text-xl laptop:text-xl laptopl:text-xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+              className="text-md tablet:text-md laptop:text-xl laptopl:text-xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
             >
               {data.headerTaglineFour}
             </h1>
